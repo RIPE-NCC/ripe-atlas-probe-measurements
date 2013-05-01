910bc35691540d2c77f7d8ea9a3170e5de44b8c7
Antony Antony <antony@ripe.net>
2013-05-01 13:23:37 +0200

    remove ping/traceroute/nslookup from perd cmdtable

--- include/cmdtable.h
+++ include/cmdtable.h
@@ -7,12 +7,7 @@ Commands for perd and ooqd
 int condmv_main(int argc, char *argv[]);
 int httpget_main(int argc, char *argv[]);
 int httppost_main(int argc, char *argv[]);
-int nslookup_main(int argc, char *argv[]);
-int ping6_main(int argc, char *argv[]);
-int ping_main(int argc, char *argv[]);
 int sslgetcert_main(int argc, char *argv[]);
-int tdig_main(int argc, char *argv[]);
-int traceroute_main(int argc, char *argv[]);
 
 static struct builtin 
 {
@@ -22,10 +17,7 @@ static struct builtin
 {
 	{ "condmv", condmv_main },
 	{ "httppost", httppost_main },
-	{ "ping6", ping6_main },
-	{ "ping", ping_main },
 	{ "sslgetcert", sslgetcert_main },
-	{ "traceroute", traceroute_main },
 	{ NULL, 0 }
 };
 
